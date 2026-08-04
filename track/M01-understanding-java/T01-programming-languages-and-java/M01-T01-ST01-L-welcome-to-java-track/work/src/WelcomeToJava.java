fd00ca44c9aac1626892344395c2f3cedbdcee8c
unknown <veeravallivenkat07@gmail.com>
2026-08-04 12:56:10 +0530

    push

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,14 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thrusday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
     }
 }
