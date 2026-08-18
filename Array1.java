c0533dd15e918ebfc0b0d1278c093eb4798f6599
unknown <veeravallivenkat07@gmail.com>
2026-08-18 11:02:19 +0530

    push

---
+++ Array1.java
@@ -0,0 +1,23 @@
+public class Array1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a[] = new int[5];
+
+        System.out.println("Enter the elements");
+        for (int i = 0; i <= a.lenght-1; i++) {
+            a[i] = scan.nextInt();
+        }
+        System.out.println("Array Elements are :");
+        for (int i = 0; i<=a.length-1; i++); {
+            System.out.println(a[i] + " ");
+        }
+        int sum = 0;
+        for (int i = 0; i<=a.length-1; i++) {
+            sum = sum + a[i];
+            System.out.println();
+            System.out.println("Sum : " + sum);
+        
+    }
+    
+}
